7f9ff971c71f3cd09a3e6361679c72bd577b18c8
Ngamfe Armel <armelngamfe@gmail.com>
2024-11-05 23:23:12 +0000

    completed task 2

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+# bookshelf/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','publication_year')
+
+    search_fields = ('title','author')
+
+    list_filter = ('publication_year','author')
+
+
+admin.site.register(Book, BookAdmin)
