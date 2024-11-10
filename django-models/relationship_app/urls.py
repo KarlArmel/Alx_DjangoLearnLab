24f52e01f695e5e1220b395b58c0015e3a617749
Ngamfe Armel <armelngamfe@gmail.com>
2024-11-10 23:05:00 +0000

    failing test

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
 
-
+["add_book/", "edit_book/"]
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
