de4090bc0008286ee857cf61a721d14dc6ec6385
Ngamfe Armel <armelngamfe@gmail.com>
2024-11-10 22:33:42 +0000

    task 3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,10 +2,12 @@
 from django.shortcuts import render,redirect
 from .models import Book
 from .models import Library
+from .models import UserProfile
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import user_passes_test
 ["relationship_app/register.html"]
 
 def list_books(request):
@@ -28,3 +30,28 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+# Helper functions to check user roles
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html', {})
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html', {})
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html', {})
