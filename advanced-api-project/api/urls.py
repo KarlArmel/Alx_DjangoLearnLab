bd9d5059c53d2a1e18d48f44dbcf94b9c3a64d1d
Ngamfe Armel <armelngamfe@gmail.com>
2024-12-01 23:37:58 +0000

    fix test

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 from . import views
-"api.urls"
+api.urls
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
