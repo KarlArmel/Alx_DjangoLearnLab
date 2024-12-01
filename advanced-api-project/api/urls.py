f53b3374e5987ffc59e5e2e0a690c0572d9c109e
Ngamfe Armel <armelngamfe@gmail.com>
2024-12-01 23:28:33 +0000

    fix failing test

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
      path('books/', views.BookListView.as_view(), name='book-list'),  # ListView and CreateView
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # DetailView
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # CreateView
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),  # UpdateView
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # DeleteView
+    path('books/update/', views.BookUpdateView.as_view(), name='book-update'),  # UpdateView
+    path('books/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # DeleteView
 
 ]
 
