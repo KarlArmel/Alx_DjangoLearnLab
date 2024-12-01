d24e4b3246966ae058be57526ead59ea95fa9a60
Ngamfe Armel <armelngamfe@gmail.com>
2024-12-01 23:52:56 +0000

    fix

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,7 +6,7 @@ from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import SearchFilter, OrderingFilter
-
+from django_filters import rest_framework
 
 
 
