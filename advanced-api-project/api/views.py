58d6710bcb73770a25684b55f23043a6df1aad40
Ngamfe Armel <armelngamfe@gmail.com>
2024-12-02 00:00:49 +0000

    fix test

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 import django_filters
+from django_filters import rest_framework
 
 class BookListView(generics.ListCreateAPIView):
     """
