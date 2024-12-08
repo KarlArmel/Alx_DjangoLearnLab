ecc425ab8013dc63328f734dc361d57d62db9b6e
karl armel <armelngamfe@gmail.com>
2024-12-08 18:26:20 +0000

    work in progress

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,13 +12,10 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path("post/<int:pk>/update/")
-     path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
-    path("comment/<int:pk>/update/")
-    path("post/<int:pk>/comments/new/")
-     path('search/', views.search_posts, name='search-posts'),
+    path('search/', views.search_posts, name='search-posts'),
     path('tags/<str:tag_name>/', views.tag_detail, name='tag-detail'),
 
 
