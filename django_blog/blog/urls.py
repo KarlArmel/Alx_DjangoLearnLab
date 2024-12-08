98b51b69f8f246559233b126f0bb125136ee3056
Ngamfe Armel <armelngamfe@gmail.com>
2024-12-08 17:26:52 +0000

    fix failing test

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
      path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path("comment/<int:pk>/update/")
+    path("post/<int:pk>/comments/new/")
 
 
 ]
